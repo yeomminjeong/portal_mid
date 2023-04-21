acf88ad197c135349162ed28000c0559ffb4fc5b
zeta <yeomfilm@gmail.com>
2023-04-21 17:06:28 +0900

    4번

--- src/test/java/kr/ac/jejunu/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/UserDaoTests.java
@@ -17,8 +17,8 @@ public class UserDaoTests {
         String name = "zeta";
         String password = "1234";
 
-        ConnectionMaker connectionMaker = new JejuConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.getUserDao();
 
         User user = userDao.findById(id);
         assertThat(user.getId(), is(id));
@@ -34,8 +34,8 @@ public class UserDaoTests {
         user.setName(name);
         user.setPassword(password);
 
-        ConnectionMaker connectionMaker = new JejuConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.getUserDao();
 
         userDao.insert(user);
         User insertedUser = userDao.findById(user.getId());
@@ -43,34 +43,6 @@ public class UserDaoTests {
         assertThat(user.getPassword(),is(password));
     }
 
-    @Test
-    public void getHalla() throws SQLException, ClassNotFoundException {
-        Long id = 1l;
-        String name = "zeta";
-        String password = "1234";
-        ConnectionMaker connectionMaker = new HallaConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-        User user = userDao.findById(id);
-        assertThat(user.getId(), is(id));
-        assertThat(user.getName(), is(name));
-        assertThat(user.getPassword(), is(password));
-    }
-    @Test
-    public void insertHalla() throws SQLException, ClassNotFoundException {
-        String name = "minjeong";
-        String password = "1111";
 
-        User user = new User();
-        user.setName(name);
-        user.setPassword(password);
-
-        ConnectionMaker connectionMaker = new HallaConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-        userDao.insert(user);
-
-        User insertedUser = userDao.findById(user.getId());
-        assertThat(user.getName(), is(name));
-        assertThat(user.getPassword(),is(password));
-    }
 
 }
